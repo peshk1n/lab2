1310d0602564385825183377cc3fa169bf0a3a93
Anastasia <keleshyan.nastya@bk.ru>
2024-09-10 13:40:25 +0300

    Add MainWindow

--- lab2/Form1.Designer.cs
+++ lab2/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.task1 = new System.Windows.Forms.Button();
+            this.task2 = new System.Windows.Forms.Button();
+            this.task3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(579, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загрузить изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // task1
+            // 
+            this.task1.Enabled = false;
+            this.task1.Location = new System.Drawing.Point(12, 96);
+            this.task1.Name = "task1";
+            this.task1.Size = new System.Drawing.Size(188, 58);
+            this.task1.TabIndex = 1;
+            this.task1.Text = "Задание 1";
+            this.task1.UseVisualStyleBackColor = true;
+            // 
+            // task2
+            // 
+            this.task2.Enabled = false;
+            this.task2.Location = new System.Drawing.Point(206, 96);
+            this.task2.Name = "task2";
+            this.task2.Size = new System.Drawing.Size(188, 58);
+            this.task2.TabIndex = 2;
+            this.task2.Text = "Задание 2";
+            this.task2.UseVisualStyleBackColor = true;
+            // 
+            // task3
+            // 
+            this.task3.Enabled = false;
+            this.task3.Location = new System.Drawing.Point(400, 96);
+            this.task3.Name = "task3";
+            this.task3.Size = new System.Drawing.Size(188, 58);
+            this.task3.TabIndex = 3;
+            this.task3.Text = "Задание 3";
+            this.task3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(600, 184);
+            this.Controls.Add(this.task3);
+            this.Controls.Add(this.task2);
+            this.Controls.Add(this.task1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button button1;
+        private Button task1;
+        private Button task2;
+        private Button task3;
     }
 }
--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -1,10 +1,34 @@
+using System.Windows.Forms;
+
 namespace lab2
 {
     public partial class Form1 : Form
     {
+        private Image image;
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image = Image.FromFile(dialog.FileName);
+                task1.Enabled = true;
+                task2.Enabled = true;
+                task3.Enabled = true;
+            }
         }
     }
 }
