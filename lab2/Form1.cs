1d8aa7e545daeaff03e81669cc6a1618a0c87a21
Anastasia <keleshyan.nastya@bk.ru>
2024-09-16 21:29:06 +0300

    Fix Task2

--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -35,9 +35,9 @@ namespace lab2
             {
                 Bitmap bitmap = new Bitmap(image);
 
-                Bitmap red = new Bitmap(bitmap.Width, bitmap.Height);
-                Bitmap green = new Bitmap(bitmap.Width, bitmap.Height);
-                Bitmap blue = new Bitmap(bitmap.Width, bitmap.Height);
+                Bitmap red_image = new Bitmap(bitmap.Width, bitmap.Height);
+                Bitmap green_image = new Bitmap(bitmap.Width, bitmap.Height);
+                Bitmap blue_image = new Bitmap(bitmap.Width, bitmap.Height);
 
                 int[] red_hist = new int[256];
                 int[] green_hist = new int[256];
@@ -48,9 +48,9 @@ namespace lab2
                     for (int y = 0; y < bitmap.Height; y++)
                     {
                         Color originalColor = bitmap.GetPixel(x, y);
-                        red.SetPixel(x, y, Color.FromArgb(originalColor.A, originalColor.R, 0, 0));
-                        green.SetPixel(x, y, Color.FromArgb(originalColor.A, 0, originalColor.G, 0));
-                        blue.SetPixel(x, y, Color.FromArgb(originalColor.A, 0, 0, originalColor.B));
+                        red_image.SetPixel(x, y, Color.FromArgb(originalColor.A, originalColor.R, 0, 0));
+                        green_image.SetPixel(x, y, Color.FromArgb(originalColor.A, 0, originalColor.G, 0));
+                        blue_image.SetPixel(x, y, Color.FromArgb(originalColor.A, 0, 0, originalColor.B));
 
                         red_hist[originalColor.R]++;
                         green_hist[originalColor.G]++;
@@ -58,44 +58,31 @@ namespace lab2
                     }
                 }
                 Bitmap hist = CreateHistogramImage(red_hist, green_hist, blue_hist);
-                ShowRGBImagesAndHistogram(red, green, blue, hist);
+                ShowRGBImagesAndHistogram(red_image, green_image, blue_image, hist);
             }
         }
 
         private Bitmap CreateHistogramImage(int[] red_hist, int[] green_hist, int[] blue_hist)
         {
-            int width = 256;  
-            int height = 100; 
+            int width = 256 * 3; 
+            int height = 100;
 
             Bitmap histogram = new Bitmap(width, height);
-            int maxRed = red_hist.Max();
-            int maxGreen = green_hist.Max();
-            int maxBlue = blue_hist.Max();
 
             using (Graphics g = Graphics.FromImage(histogram))
             {
                 g.Clear(Color.White);
 
-                List<PointF> red_points = new List<PointF>();
-                List<PointF> green_points = new List<PointF>();
-                List<PointF> blue_points = new List<PointF>();
-
-
                 for (int i = 0; i < 256; i++)
                 {
-                    int redHeight = height - (int)(height * red_hist[i] / (float)maxRed);
-                    int greenHeight = height - (int)(height * green_hist[i] / (float)maxGreen);
-                    int blueHeight = height - (int)(height * blue_hist[i] / (float)maxBlue);
-                    red_points.Add(new PointF(i, redHeight));
-                    green_points.Add(new PointF(i, greenHeight));
-                    blue_points.Add(new PointF(i, blueHeight));
-                    
+                    int hr = height - (int)(height * red_hist[i] / (float)red_hist.Max());
+                    int hg = height - (int)(height * green_hist[i] / (float)green_hist.Max());
+                    int hb = height - (int)(height * blue_hist[i] / (float)blue_hist.Max());
+                    g.DrawLine(Pens.Red, i * 3, height, i * 3, hr);
+                    g.DrawLine(Pens.Green, i * 3 + 1, height, i * 3 + 1, hg);
+                    g.DrawLine(Pens.Blue, i * 3 + 2, height, i * 3 + 2, hb);
                 }
-                g.DrawLines(Pens.Red, red_points.ToArray());
-                g.DrawLines(Pens.Green, green_points.ToArray());
-                g.DrawLines(Pens.Blue, blue_points.ToArray());
             }
-
             return histogram;
         }
 
@@ -103,7 +90,16 @@ namespace lab2
         {
             Form channelsForm = new Form();
             channelsForm.Text = "RGB Channels and Histogram";
-            channelsForm.Size = new Size(1600, 500); 
+            channelsForm.Size = new Size(1600, 800);
+
+            Panel imagesPanel = new Panel();
+            Panel histogramPanel = new Panel();
+
+            imagesPanel.Dock = DockStyle.Top;
+            histogramPanel.Dock = DockStyle.Bottom;
+
+            imagesPanel.Height = channelsForm.ClientSize.Height * 2 / 3; 
+            histogramPanel.Height = channelsForm.ClientSize.Height / 3;  
 
             PictureBox redPictureBox = new PictureBox();
             redPictureBox.Image = redImage;
@@ -117,45 +113,50 @@ namespace lab2
             bluePictureBox.Image = blueImage;
             bluePictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
 
+            imagesPanel.Controls.Add(redPictureBox);
+            imagesPanel.Controls.Add(greenPictureBox);
+            imagesPanel.Controls.Add(bluePictureBox);
+
             PictureBox histogramPictureBox = new PictureBox();
             histogramPictureBox.Image = histogramImage;
             histogramPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
 
-            channelsForm.Controls.Add(redPictureBox);
-            channelsForm.Controls.Add(greenPictureBox);
-            channelsForm.Controls.Add(bluePictureBox);
-            channelsForm.Controls.Add(histogramPictureBox);
+            histogramPanel.Controls.Add(histogramPictureBox);
+
+            channelsForm.Controls.Add(imagesPanel);
+            channelsForm.Controls.Add(histogramPanel);
 
             channelsForm.Resize += (s, e) =>
             {
-                ResizePictureBoxes(channelsForm, redPictureBox, greenPictureBox, bluePictureBox, histogramPictureBox);
+                ResizeComponents(channelsForm, imagesPanel, histogramPanel, redPictureBox, greenPictureBox, bluePictureBox, histogramPictureBox);
             };
 
-            ResizePictureBoxes(channelsForm, redPictureBox, greenPictureBox, bluePictureBox, histogramPictureBox);
+            ResizeComponents(channelsForm, imagesPanel, histogramPanel, redPictureBox, greenPictureBox, bluePictureBox, histogramPictureBox);
 
             channelsForm.ShowDialog();
         }
 
-        private void ResizePictureBoxes(Form form, PictureBox redPictureBox, PictureBox greenPictureBox, PictureBox bluePictureBox, PictureBox histogramPictureBox)
+        private void ResizeComponents(Form form, Panel imagesPanel, Panel histogramPanel, PictureBox redPictureBox, PictureBox greenPictureBox, PictureBox bluePictureBox, PictureBox histogramPictureBox)
         {
             int formWidth = form.ClientSize.Width;
-            int formHeight = form.ClientSize.Height;
+            int imagesPanelHeight = form.ClientSize.Height * 2 / 3;
+            int histogramPanelHeight = form.ClientSize.Height / 3;
 
-            int pictureBoxWidth = formWidth / 4;
-            int pictureBoxHeight = formHeight;
+            imagesPanel.Height = imagesPanelHeight;
+            histogramPanel.Height = histogramPanelHeight;
 
-            redPictureBox.Size = new Size(pictureBoxWidth, pictureBoxHeight);
+            int pictureBoxWidth = formWidth / 3;
+            redPictureBox.Size = new Size(pictureBoxWidth, imagesPanelHeight);
+            greenPictureBox.Size = new Size(pictureBoxWidth, imagesPanelHeight);
+            bluePictureBox.Size = new Size(pictureBoxWidth, imagesPanelHeight);
+
             redPictureBox.Location = new Point(0, 0);
-
-            greenPictureBox.Size = new Size(pictureBoxWidth, pictureBoxHeight);
             greenPictureBox.Location = new Point(pictureBoxWidth, 0);
-
-            bluePictureBox.Size = new Size(pictureBoxWidth, pictureBoxHeight);
             bluePictureBox.Location = new Point(pictureBoxWidth * 2, 0);
 
-            histogramPictureBox.Size = new Size(pictureBoxWidth, pictureBoxHeight);
-            histogramPictureBox.Location = new Point(pictureBoxWidth * 3, 0);
+            histogramPictureBox.Size = new Size(formWidth, histogramPanelHeight);
         }
+
 
         private void task3_Click(object sender, EventArgs e)
         {
